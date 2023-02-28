8f0764925c633ce915ccb6a2c63f6cf26a6745d7
Damir <leheln16@gmail.com>
2023-02-28 15:18:54 +0500

    Step

--- hello/firstapp/models.py
+++ hello/firstapp/models.py
@@ -20,3 +20,13 @@ class Product(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     price = models.IntegerField()
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+
+class Account(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                primary_key=True)
